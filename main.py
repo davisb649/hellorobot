8c5827807b684bd583472a354fe67df32589f281
davisb <brianpatrick.brian517@gmail.com>
2017-09-13 14:33:27 -0400

    hello robot

---
+++ main.py
@@ -0,0 +1,18 @@
+from gopigo import *
+import time
+
+class Piggy(object):
+
+    def __init__(self):
+
+        print("Hello, I'm your robot")
+        self.dance()
+
+    def dance(self):
+        fwd()
+        time.sleep(1.5)
+        right_rot()
+        time.sleep(.5)
+        stop()
+
+teko = Piggy()
